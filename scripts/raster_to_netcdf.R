9dd4231b245ffc00ae79f33ceba788b62690e682
Alexander Barth <barth.alexander@gmail.com>
2020-10-19 15:50:46 +0200

    use WGS84

--- scripts/raster_to_netcdf.R
+++ scripts/raster_to_netcdf.R
@@ -1,7 +1,7 @@
-# Run as:
-# Rscript raster_to_netcdf.R
+# Run in ../data as:
+# Rscript ../script/raster_to_netcdf.R
 
-# get EUSM2019_substrate.gdb from EMODnet Seabed habitat
+# get data/EUSM2019_substrate.gdb from EMODnet Seabed habitat
 
 # install.package("sf")
 # install.packages("raster")
@@ -17,8 +17,12 @@ library(ncdf4)
 library(RNetCDF)
 
 seds<-sf::st_read(dsn = "EUSM2019_substrate.gdb", layer = "EUSM2019_Arctic_Atlantic_substrate")
-r<-raster(seds,res=10000)
-bb<-fasterize(seds,r,by="Substrate")
+#r<-raster(seds,res=10000)
+#bb<-fasterize(seds,r,by="Substrate")
+
+seds_lonlat <- st_transform(seds, "+proj=longlat +datum=WGS84 +no_defs")
+r<-raster(seds_lonlat,res=0.05)
+bb<-fasterize(seds_lonlat,r,by="Substrate")
 
 writeRaster(bb, "substrate.nc", overwrite=TRUE, format="CDF", bylayer=TRUE,suffix='names')
 
